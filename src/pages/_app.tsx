f5658cac15074349b67e87645c33d8cd5fc23ba0
Breno Baptista <brenobaptista@protonmail.com>
2021-04-05 04:15:37 -0300

    Fixed title import

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,7 +2,7 @@ import Head from 'next/head'
 import { AppProps } from 'next/app'
 import 'tailwindcss/tailwind.css'
 
-import siteTitle from './_document'
+import { siteTitle } from './_document'
 
 const MyApp = ({ Component, pageProps }: AppProps): JSX.Element => (
   <>
