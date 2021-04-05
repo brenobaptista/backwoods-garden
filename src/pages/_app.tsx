70090dcce29e9454084fc5bc56cc6b8171e1b2c3
Breno Baptista <brenobaptista@protonmail.com>
2021-04-05 03:54:35 -0300

    Added custom _document

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,16 @@
+import Head from 'next/head'
 import { AppProps } from 'next/app'
 import 'tailwindcss/tailwind.css'
 
+import siteTitle from './_document'
+
 const MyApp = ({ Component, pageProps }: AppProps): JSX.Element => (
-  <Component {...pageProps} />
+  <>
+    <Head>
+      <title>{siteTitle}</title>
+    </Head>
+    <Component {...pageProps} />
+  </>
 )
 
 export default MyApp
