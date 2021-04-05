a39edbb115a1ef8731e38a052cc71a10eb69c132
Breno Baptista <brenobaptista@protonmail.com>
2021-04-05 02:52:28 -0300

    Set resposive breakpoint to lg

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,18 +1,18 @@
 const Hero = (): JSX.Element => (
-  <header className='py-40 md:py-60 bg-green-100'>
-    <div className='mx-20 md:mx-40 lg:mx-60 text-5xl md:text-6xl text-center font-medium'>
+  <header className='py-40 lg:py-60 bg-green-100'>
+    <div className='mx-20 lg:mx-60 text-5xl lg:text-6xl text-center font-medium'>
       Encontre vasos, plantas ornamentais e artigos de madeira artesanal.
     </div>
     <div className='flex justify-center mt-20'>
       <button
         type='button'
-        className='w-40 md:w-64 text-base md:text-xl mr-2 md:mr-4 shadow-lg rounded p-4 border-2 border-green-600 hover:border-green-700 bg-green-600 hover:bg-green-700 text-white'
+        className='w-40 lg:w-64 text-base lg:text-xl mr-2 lg:mr-4 shadow-lg rounded p-4 border-2 border-green-600 hover:border-green-700 bg-green-600 hover:bg-green-700 text-white'
       >
         Ver Produtos
       </button>
       <button
         type='button'
-        className='w-40 md:w-64 text-base md:text-xl ml-2 md:mr-4 shadow-lg rounded p-4 border-2 border-green-600 hover:border-green-800 text-green-600 hover:text-green-800'
+        className='w-40 lg:w-64 text-base lg:text-xl ml-2 lg:mr-4 shadow-lg rounded p-4 border-2 border-green-600 hover:border-green-800 text-green-600 hover:text-green-800'
       >
         Abrir WhatsApp
       </button>
