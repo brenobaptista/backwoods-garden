d3d7524b7bbe5d7ada786bf5263d5d4c8d5deaf7
Breno Baptista <brenobaptista@protonmail.com>
2021-04-06 01:00:45 -0300

    Added testimonials section

---
+++ src/components/Testimonials.tsx
@@ -0,0 +1,45 @@
+const Testimonials = (): JSX.Element => (
+  <section className='bg-gray-100'>
+    <div className='container mx-auto px-6 py-20'>
+      <h1 className='text-4xl font-bold text-center mb-8'>Avaliações</h1>
+      <div className='flex flex-wrap'>
+        <div className='w-full md:w-1/3 px-2 mb-4'>
+          <div className='bg-white rounded shadow py-2'>
+            <p className='text-base px-6 mb-5'>
+              Ótimo atendimento, a Eva é super atenciosa e ama o que faz, sem
+              contar a variedade! Adoro esse lugar 😁👏
+            </p>
+            <p className='text-gray-500 text-xs md:text-sm px-6'>
+              Fernanda Tessaroli
+            </p>
+          </div>
+        </div>
+        <div className='w-full md:w-1/3 px-2 mb-4'>
+          <div className='bg-white rounded shadow py-2'>
+            <p className='text-base px-6 mb-5'>
+              Local com diversas opções de plantas! Fiquei encantada! Tem muitas
+              opções de vasos também. O adubo é de excelente qualidade e é
+              vendido em diversos tamanhos de sacas. Aceita cartões de débito e
+              crédito (o que facilita pra quem não anda com dinheiro hoje em
+              dia).
+            </p>
+            <p className='text-gray-500 text-xs md:text-sm px-6'>Dhayana</p>
+          </div>
+        </div>
+        <div className='w-full md:w-1/3 px-2 mb-4'>
+          <div className='bg-white rounded shadow py-2'>
+            <p className='text-base px-6 mb-5'>
+              Eva tem um excelente atendimento e muito amor pelo que faz,
+              realmente ela transfere todo esse amor e carinho para as plantas.
+            </p>
+            <p className='text-gray-500 text-xs md:text-sm px-6'>
+              Silvia Helena Abreu
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+)
+
+export default Testimonials
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import Hero from '../components/Hero'
 import Navbar from '../components/Navbar'
+import Testimonials from '../components/Testimonials'
 
 const Home = (): JSX.Element => (
   <>
@@ -7,7 +8,7 @@ const Home = (): JSX.Element => (
     <Hero />
     <section>Produtos</section>
     <section>Vídeos</section>
-    <section>Avaliações (Testemunho) (review do Google)</section>
+    <Testimonials />
     <footer>
       Redes sociais (WhatsApp, Facebook, Instagram e talvez Tik Tok), horário de
       funcionamento e endereço. Abaixo colocar feito por Breno Baptista
