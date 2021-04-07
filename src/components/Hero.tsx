2a80cc60ace51f5d08608af8577d8372067452a2
Breno Baptista <brenobaptista@protonmail.com>
2021-04-06 23:00:06 -0300

    Updated WhatsApp link

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -12,7 +12,7 @@ const Hero = (): JSX.Element => (
       </a>
       <a
         className='w-40 lg:w-64 text-base lg:text-xl ml-2 lg:mr-4 shadow-lg rounded p-4 text-center border-2 border-green-600 hover:bg-green-600 text-green-600 hover:text-white transition'
-        href='#whatsapp'
+        href='https://api.whatsapp.com/send?phone=5585988709845&text=Ol%C3%A1!%20Peguei%20seu%20telefone%20no%20site%20do%20Jardim%20Caipira.'
       >
         Abrir WhatsApp
       </a>
