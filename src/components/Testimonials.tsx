94bdd9c88a4b7e5285bb00c6bd445c79c3dc503d
Breno Baptista <brenobaptista@protonmail.com>
2021-04-07 19:27:57 -0300

    Improved testimonials styles

--- src/components/Testimonials.tsx
+++ src/components/Testimonials.tsx
@@ -1,10 +1,10 @@
 const Testimonials = (): JSX.Element => (
   <section className='bg-gray-100' id='avaliacoes'>
     <div className='container mx-auto px-6 py-20'>
-      <h1 className='text-4xl font-bold text-center mb-8'>Avaliações</h1>
+      <h1 className='text-4xl font-bold text-center mb-12'>Avaliações</h1>
       <div className='flex flex-wrap'>
         <div className='w-full md:w-1/3 px-2 mb-4'>
-          <div className='bg-white rounded shadow-md py-2'>
+          <div className='bg-white rounded shadow-md py-4'>
             <p className='text-base px-6 mb-5'>
               Ótimo atendimento, a Eva é super atenciosa e ama o que faz, sem
               contar a variedade! Adoro esse lugar 😁👏
@@ -15,7 +15,7 @@ const Testimonials = (): JSX.Element => (
           </div>
         </div>
         <div className='w-full md:w-1/3 px-2 mb-4'>
-          <div className='bg-white rounded shadow-md py-2'>
+          <div className='bg-white rounded shadow-md py-4'>
             <p className='text-base px-6 mb-5'>
               Local com diversas opções de plantas! Fiquei encantada! Tem muitas
               opções de vasos também. O adubo é de excelente qualidade e é
@@ -27,7 +27,7 @@ const Testimonials = (): JSX.Element => (
           </div>
         </div>
         <div className='w-full md:w-1/3 px-2 mb-4'>
-          <div className='bg-white rounded shadow-md py-2'>
+          <div className='bg-white rounded shadow-md py-4'>
             <p className='text-base px-6 mb-5'>
               Eva tem um excelente atendimento e muito amor pelo que faz,
               realmente ela transfere todo esse amor e carinho para as plantas.
