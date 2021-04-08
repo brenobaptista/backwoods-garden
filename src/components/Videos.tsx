a5e1283e95ec97a69826d2dca51c9aef7b27409d
Breno Baptista <brenobaptista@protonmail.com>
2021-04-08 01:10:55 -0300

    Added thumbnails for videos

--- src/components/Videos.tsx
+++ src/components/Videos.tsx
@@ -1,43 +1,45 @@
+import Image from 'next/image'
+
 const Videos = (): JSX.Element => (
   <section className='bg-green-100' id='videos'>
     <div className='container mx-auto px-6 py-20'>
       <h1 className='text-4xl font-bold text-center mb-12'>Vídeos</h1>
       <div className='flex flex-wrap'>
-        <div className='w-full md:w-1/2 px-2 mb-4'>
-          <div className='bg-white rounded shadow-md py-2'>
+        <div className='w-full md:w-1/2 px-4 mb-8 md:mb-0'>
+          <div className='bg-white rounded shadow-md'>
             <a
               href='https://youtu.be/v3ur8ny3R70'
               target='_blank'
               rel='noreferrer noopener'
             >
-              <img
-                width='640'
-                height='320'
-                src='https://youtu.be/v3ur8ny3R70'
-                alt='COMO TRATAR RAÍZES PODRES COM A EVA DO JARDIM CAIPIRA!'
+              <Image
+                src='/images/como-tratar-raizes-podres.jpg'
+                alt='Como tratar raízes podres'
+                width={728}
+                height={410}
               />
-              <div className='text-lg text-center font-semibold'>
+              <p className='text-lg lg:text-2xl text-center font-semibold pt-2 pb-4'>
                 Como tratar raízes podres
-              </div>
+              </p>
             </a>
           </div>
         </div>
-        <div className='w-full md:w-1/2 px-2 mb-4'>
-          <div className='bg-white rounded shadow-md py-2'>
+        <div className='w-full md:w-1/2 px-4 mb-8 md:mb-0'>
+          <div className='bg-white rounded shadow-md'>
             <a
               href='https://youtu.be/uKapAnCC9Bg'
               target='_blank'
               rel='noreferrer noopener'
             >
-              <img
-                width='640'
-                height='320'
-                src='https://youtu.be/uKapAnCC9Bg'
-                alt='RESULTADO DA ROSA DO DESERTO JARDIM CAIPIRA'
+              <Image
+                src='/images/resultado-do-tratamento.jpg'
+                alt='Resultado do tratamento'
+                width={728}
+                height={410}
               />
-              <div className='text-lg text-center font-semibold'>
+              <p className='text-lg lg:text-2xl text-center font-semibold pt-2 pb-4'>
                 Resultado do tratamento
-              </div>
+              </p>
             </a>
           </div>
         </div>
