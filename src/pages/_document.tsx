c33b948509188a416f237594593d62d283c350f8
Breno Baptista <brenobaptista@protonmail.com>
2021-04-14 15:56:48 -0300

    Added Google Search Console metatag

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -18,6 +18,10 @@ export default class MyDocument extends Document {
               siteTitle
             )}.png?theme=light&md=0&fontSize=200px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fvercel-triangle-black.svg&widths=50&heights=50`}
           />
+          <meta
+            name='google-site-verification'
+            content='_cd7mw2YWYlX1KdSTwEDVxDz2n5eWnW87t8mo4WdKF8'
+          />
           <meta name='og:title' content={siteTitle} />
           <meta name='twitter:card' content='summary_large_image' />
         </Head>
