69533f0d2dcd41dd909bdc0a64ee4525b87032f0
Breno Baptista <brenobaptista@protonmail.com>
2021-09-02 00:40:34 -0300

    Created two new icons

--- src/components/Theme.tsx
+++ src/components/Theme.tsx
@@ -1,5 +1,8 @@
 import { useState, useEffect } from 'react'
 
+import Moon from './icons/Moon'
+import Sun from './icons/Sun'
+
 const Theme = (): JSX.Element => {
   const [darkMode, setDarkMode] = useState(false)
 
@@ -33,35 +36,9 @@ const Theme = (): JSX.Element => {
       onClick={() => setDarkMode(!darkMode)}
     >
       {darkMode ? (
-        <svg
-          className='w-6 h-6'
-          xmlns='http://www.w3.org/2000/svg'
-          fill='none'
-          viewBox='0 0 24 24'
-          stroke='currentColor'
-        >
-          <path
-            strokeLinecap='round'
-            strokeLinejoin='round'
-            strokeWidth={2}
-            d='M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z'
-          />
-        </svg>
+        <Moon width={24} height={24} />
       ) : (
-        <svg
-          className='w-6 h-6'
-          xmlns='http://www.w3.org/2000/svg'
-          fill='none'
-          viewBox='0 0 24 24'
-          stroke='currentColor'
-        >
-          <path
-            strokeLinecap='round'
-            strokeLinejoin='round'
-            strokeWidth={2}
-            d='M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z'
-          />
-        </svg>
+        <Sun width={24} height={24} />
       )}
     </button>
   )
