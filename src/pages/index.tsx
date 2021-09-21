b2b8312a085e0bde603289b0c23f58f316d04448
Breno Baptista <brenobaptista@protonmail.com>
2021-09-21 20:23:58 -0300

    Added transition to theme change

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,7 +6,7 @@ import Testimonials from '../components/Testimonials'
 import Videos from '../components/Videos'
 
 const Home = (): JSX.Element => (
-  <div className='text-gray-800 dark:text-gray-200 font-sans antialiased'>
+  <div className='text-gray-800 dark:text-gray-200 transition-colors font-sans antialiased'>
     <Navbar />
     <Hero />
     <Products />
