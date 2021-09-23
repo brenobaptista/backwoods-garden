228aa67ed59bb62718908063ce1af9f83bd9b480
Breno Baptista <brenobaptista@protonmail.com>
2021-09-22 23:19:57 -0300

    Updated og:image

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -17,7 +17,7 @@ export default class MyDocument extends Document {
           />
           <meta
             property='og:image'
-            content={`https://og-image.now.sh/${encodeURI(
+            content={`https://og-image.vercel.app/${encodeURI(
               siteTitle
             )}.png?theme=light&md=0&fontSize=200px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fvercel-triangle-black.svg&widths=50&heights=50`}
           />
