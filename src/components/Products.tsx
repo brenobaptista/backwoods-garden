ae7b6e526673848efd75abd801b4dc7763b7df56
Breno Baptista <brenobaptista@protonmail.com>
2021-09-28 21:37:19 -0300

    Used grid instead of flex for products

--- src/components/Products.tsx
+++ src/components/Products.tsx
@@ -8,118 +8,101 @@ const Products = (): JSX.Element => (
         Visite nossa loja física ou acesse nossas redes sociais para mais
         informações.
       </p>
-      <div className='flex flex-wrap'>
-        <div className='w-full md:w-1/3 p-4'>
-          <div className='group flex justify-center items-center bg-black dark:bg-gray-800 text-gray-200 rounded'>
-            <div className='group-hover:opacity-25 transition motion-reduce:transition-none duration-300 ease-in-out'>
-              <img
-                src='/images/products/cactos.jpg'
-                alt='Cactos'
-                className='rounded'
-                width={480}
-                height={480}
-              />
-            </div>
-            <div className='absolute opacity-0 group-hover:opacity-100 transition motion-reduce:transition-none duration-300 ease-in-out'>
-              <span className='text-3xl font-bold tracking-wider leading-relaxed'>
-                Cactos
-              </span>
-            </div>
+      <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8'>
+        <div className='group flex justify-center items-center bg-black dark:bg-gray-800 text-gray-200'>
+          <div className='group-hover:opacity-25 transition motion-reduce:transition-none duration-300 ease-in-out'>
+            <img
+              src='/images/products/cactos.jpg'
+              alt='Cactos'
+              className='rounded'
+              width={480}
+              height={480}
+            />
+          </div>
+          <div className='absolute opacity-0 group-hover:opacity-100 transition motion-reduce:transition-none duration-300 ease-in-out'>
+            <span className='text-3xl font-bold tracking-wider leading-relaxed'>
+              Cactos
+            </span>
           </div>
         </div>
-
-        <div className='w-full md:w-1/3 p-4'>
-          <div className='group flex justify-center items-center bg-black dark:bg-gray-800 text-gray-200 rounded'>
-            <div className='group-hover:opacity-25 transition motion-reduce:transition-none duration-300 ease-in-out'>
-              <img
-                src='/images/products/decoracao.jpg'
-                alt='Decoração'
-                className='rounded'
-                width={480}
-                height={480}
-              />
-            </div>
-            <div className='absolute opacity-0 group-hover:opacity-100 transition motion-reduce:transition-none duration-300 ease-in-out'>
-              <span className='text-3xl font-bold tracking-wider leading-relaxed'>
-                Decoração
-              </span>
-            </div>
+        <div className='group flex justify-center items-center bg-black dark:bg-gray-800 text-gray-200'>
+          <div className='group-hover:opacity-25 transition motion-reduce:transition-none duration-300 ease-in-out'>
+            <img
+              src='/images/products/decoracao.jpg'
+              alt='Decoração'
+              className='rounded'
+              width={480}
+              height={480}
+            />
+          </div>
+          <div className='absolute opacity-0 group-hover:opacity-100 transition motion-reduce:transition-none duration-300 ease-in-out'>
+            <span className='text-3xl font-bold tracking-wider leading-relaxed'>
+              Decoração
+            </span>
           </div>
         </div>
-
-        <div className='w-full md:w-1/3 p-4'>
-          <div className='group flex justify-center items-center bg-black dark:bg-gray-800 text-gray-200 rounded'>
-            <div className='group-hover:opacity-25 transition motion-reduce:transition-none duration-300 ease-in-out'>
-              <img
-                src='/images/products/suculenta.jpg'
-                alt='Suculenta'
-                className='rounded'
-                width={480}
-                height={480}
-              />
-            </div>
-            <div className='absolute opacity-0 group-hover:opacity-100 transition motion-reduce:transition-none duration-300 ease-in-out'>
-              <span className='text-3xl font-bold tracking-wider leading-relaxed'>
-                Suculenta
-              </span>
-            </div>
+        <div className='group flex justify-center items-center bg-black dark:bg-gray-800 text-gray-200'>
+          <div className='group-hover:opacity-25 transition motion-reduce:transition-none duration-300 ease-in-out'>
+            <img
+              src='/images/products/suculenta.jpg'
+              alt='Suculenta'
+              className='rounded'
+              width={480}
+              height={480}
+            />
+          </div>
+          <div className='absolute opacity-0 group-hover:opacity-100 transition motion-reduce:transition-none duration-300 ease-in-out'>
+            <span className='text-3xl font-bold tracking-wider leading-relaxed'>
+              Suculenta
+            </span>
           </div>
         </div>
-
-        <div className='w-full md:w-1/3 p-4'>
-          <div className='group flex justify-center items-center bg-black dark:bg-gray-800 text-gray-200 rounded'>
-            <div className='group-hover:opacity-25 transition motion-reduce:transition-none duration-300 ease-in-out'>
-              <img
-                src='/images/products/orquidea.jpg'
-                alt='Orquídea'
-                className='rounded'
-                width={480}
-                height={480}
-              />
-            </div>
-            <div className='absolute opacity-0 group-hover:opacity-100 transition motion-reduce:transition-none duration-300 ease-in-out'>
-              <span className='text-3xl font-bold tracking-wider leading-relaxed'>
-                Orquídea
-              </span>
-            </div>
+        <div className='group flex justify-center items-center bg-black dark:bg-gray-800 text-gray-200'>
+          <div className='group-hover:opacity-25 transition motion-reduce:transition-none duration-300 ease-in-out'>
+            <img
+              src='/images/products/orquidea.jpg'
+              alt='Orquídea'
+              className='rounded'
+              width={480}
+              height={480}
+            />
+          </div>
+          <div className='absolute opacity-0 group-hover:opacity-100 transition motion-reduce:transition-none duration-300 ease-in-out'>
+            <span className='text-3xl font-bold tracking-wider leading-relaxed'>
+              Orquídea
+            </span>
           </div>
         </div>
-
-        <div className='w-full md:w-1/3 p-4'>
-          <div className='group flex justify-center items-center bg-black dark:bg-gray-800 text-gray-200 rounded'>
-            <div className='group-hover:opacity-25 transition motion-reduce:transition-none duration-300 ease-in-out'>
-              <img
-                src='/images/products/gloxinia.jpg'
-                alt='Gloxínia'
-                className='rounded'
-                width={480}
-                height={480}
-              />
-            </div>
-            <div className='absolute opacity-0 group-hover:opacity-100 transition motion-reduce:transition-none duration-300 ease-in-out'>
-              <span className='text-3xl font-bold tracking-wider leading-relaxed'>
-                Gloxínia
-              </span>
-            </div>
+        <div className='group flex justify-center items-center bg-black dark:bg-gray-800 text-gray-200'>
+          <div className='group-hover:opacity-25 transition motion-reduce:transition-none duration-300 ease-in-out'>
+            <img
+              src='/images/products/gloxinia.jpg'
+              alt='Gloxínia'
+              className='rounded'
+              width={480}
+              height={480}
+            />
+          </div>
+          <div className='absolute opacity-0 group-hover:opacity-100 transition motion-reduce:transition-none duration-300 ease-in-out'>
+            <span className='text-3xl font-bold tracking-wider leading-relaxed'>
+              Gloxínia
+            </span>
           </div>
         </div>
-
-        <div className='w-full md:w-1/3 p-4'>
-          <div className='group flex justify-center items-center bg-black dark:bg-gray-800 text-gray-200 rounded'>
-            <div className='group-hover:opacity-25 transition motion-reduce:transition-none duration-300 ease-in-out'>
-              <img
-                src='/images/products/bungavile.jpg'
-                alt='Bungavile'
-                className='rounded'
-                width={480}
-                height={480}
-              />
-            </div>
-            <div className='absolute opacity-0 group-hover:opacity-100 transition motion-reduce:transition-none duration-300 ease-in-out'>
-              <span className='text-3xl font-bold tracking-wider leading-relaxed'>
-                Bungavile
-              </span>
-            </div>
+        <div className='group flex justify-center items-center bg-black dark:bg-gray-800 text-gray-200'>
+          <div className='group-hover:opacity-25 transition motion-reduce:transition-none duration-300 ease-in-out'>
+            <img
+              src='/images/products/bungavile.jpg'
+              alt='Bungavile'
+              className='rounded'
+              width={480}
+              height={480}
+            />
+          </div>
+          <div className='absolute opacity-0 group-hover:opacity-100 transition motion-reduce:transition-none duration-300 ease-in-out'>
+            <span className='text-3xl font-bold tracking-wider leading-relaxed'>
+              Bungavile
+            </span>
           </div>
         </div>
       </div>
